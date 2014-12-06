5572ce78da7fb43ef06c4d862469e7b1f272deae
Kevin Buckley <kbuck210@gmail.com>
2014-12-05 19:53:09 -0500

    Updated with WSGI Server

--- Kevin_Buckley.py
+++ Kevin_Buckley.py
@@ -4,6 +4,7 @@
 
 import sqlite3
 from flask import Flask, render_template
+from wsgiref.simple_server import make_server
 
 ##
 ##  Sqlite connection Methods (Retrieve & Update EXAM table)
@@ -24,8 +25,11 @@ def getExamData():
 def updateExamData(name, exam_num):
     # Check that both the student name and the exam number have been provided
     if name == None or exam_num == None:
-        # Throw an exception if they have not been defined
-        raise Exception("Input Parameters not Fully Defined")
+        # Throw an exception if the form doesn't have defined parameters
+        raise Exception("Form input fields incorrectly defined")
+    elif not name or not exam_num:
+        # Throw a different exception if the returned values are empty
+        raise Exception("User input incomplete")
     else:
         sql = sqlite3.connect('final_exam-copy.sqlite')
         # Use with to handle closing database 
@@ -35,10 +39,71 @@ def updateExamData(name, exam_num):
                 cur.execute("UPDATE EXAM SET StudentName=? WHERE Number=?",(str(name), str(exam_num)))
                 sql.commit()
             except sqlite3.Error as e:
-                print("Failed to update EXAM table with values {} and {}".format(name, exam_num))               
-                print(e)
+                print("Failed to update table with message: {}".format(str(e)))
+                raise Exception("Failed to update EXAM table with values {} and {}".format(name, exam_num))               
             else:
                 print("Updated EXAM table with values {} and {}".format(name, exam_num))
-        
 
-updateExamData('Kevin','5')
+# SQLite database method tests        
+#updateExamData('Kevin','5')
+
+##
+##  WSGI Server
+##
+def defineServer(environ, start_response):
+    message = ""
+    status = '200 OK'
+    headers = [('Content-type', 'html; charset=utf-8')]
+    start_response(status, headers)
+    if(environ['REQUEST_METHOD'] == 'POST'):
+        request_body_size = int(environ['CONTENT_LENGTH'])
+        request_body = environ['wsgi.input'].read(request_body_size)
+        
+        # Create variables to store name & exam_num, default to None
+        exam_num = None
+        name = None
+        # Get the form values submitted by the user
+        form_vals = get_form_vals(request_body)
+        for item in form_vals.keys():
+            # check the value of the key
+            if item.lower() == "exam_num".lower():
+                exam_num = form_vals[item]
+            elif item.lower() == "name".lower():
+                name = form_vals[item]
+            else:
+                pass
+        
+        # call the updateExamData Method to update the values, catching exceptions
+        try:
+            updateExamData(name, exam_num)
+        except Exception as e:
+            message += "<br>Submission not processed: {}<br/>".format(str(e))
+        
+    message += "<h1>FINAL EXAM</h1>"
+    message += "<h2>Add Record:</h2>"
+    message += "<form method='POST'><br>Exam Number:<input type=text name='exam_num'>"
+    message += "<br><br>Student Name:<input type=text name='name'>"
+    message += "<br><br><input type='submit' name='Submit' ></form>"
+
+    # Call the method to read the table data and output to screen
+    data = getExamData()
+    # Data is list of tuples in order (exam_num, name)
+    message += '<br><table width="25%">'
+    message += '<tr><td>Exam Number:</td><td>Student Name:</td></tr>'
+    for entry in data:
+        message += '<tr><td>{number}</td><td>{student}</td></tr>'.format(number=entry[0], student=entry[1])
+    message += '</table><br/>'
+    
+    return[bytes(message,'utf-8')]
+
+def get_form_vals(post_str):
+    form_vals = {item.split("=")[0]: item.split("=")[1] for item in post_str.decode().split("&")}
+    return form_vals
+
+httpd = make_server('', 8000, defineServer)
+print("Serving on port 8000...")
+httpd.serve_forever()
+
+##
+##  Flask Application for Visualization
+##
